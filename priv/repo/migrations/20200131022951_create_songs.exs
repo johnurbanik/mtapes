0cddc5cf24cb727d3e39cff880087bc4cf225382
John Urbanik <johnurbanik@gmail.com>
2020-01-30 21:57:50 -0500

    Fix migrations

--- priv/repo/migrations/20200131022951_create_songs.exs
+++ priv/repo/migrations/20200131022951_create_songs.exs
@@ -1,22 +1,32 @@
 defmodule Mtapes.Repo.Migrations.CreateSongs do
   use Ecto.Migration
 
-  def change do
+  def up do
     create table(:songs) do
       add :name, :string
       add :artist, :string
-      spotify_id, :string
+      add :spotify_id, :string
     end
 
-
+    drop constraint("playlists", "playlists_user_id_fkey")
     alter table(:playlists) do
-      alter :user_id, references(:users, on_delete: :delete_all)
+      modify :user_id, references(:users, on_delete: :delete_all)
     end
 
-
-
-    create index(:songs, [:playlist_id])
     create unique_index(:playlists, [:spotify_id])
     create unique_index(:songs, [:spotify_id])
+  end
+
+  def down do
+    drop table(:songs)
+
+    drop constraint("playlists", "playlists_user_id_fkey")
+    alter table(:playlists) do
+      modify :user_id, references(:users, on_delete: :nothing)
+    end
+
+    drop index(:songs, [:playlist_id])
+    drop unique_index(:playlists, [:spotify_id])
+    drop unique_index(:songs, [:spotify_id])
   end
 end
